de7b51fb65b6afafdc1bdc1d68c41b075c92d504
unknown <njokuiyke01@gmail.com>
2022-12-12 13:48:37 -0500

    Added authentication

--- CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CityInfo.API.Models
-{
-    public class PointOfInterestForCreationDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
